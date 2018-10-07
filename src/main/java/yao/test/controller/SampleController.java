4c756342bfcd763fd44591515a30522a6b38037f
yaohuiyan <yaohyan@126.com>
2018-10-07 22:58:33 +0800

    thrid

--- src/main/java/yao/test/controller/SampleController.java
+++ src/main/java/yao/test/controller/SampleController.java
@@ -4,11 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import feign.Feign;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
-import yao.test.remote.Test;
-
 @Controller
 public class SampleController {
 
@@ -18,13 +13,13 @@ public class SampleController {
 		return "Hello World!" + System.currentTimeMillis();
 	}
 
-	@RequestMapping("/login")
-	@ResponseBody
-	public String loginPage() {
-		Test service = Feign.builder()
-				// .encoder(new JacksonEncoder())
-				// .decoder(new JacksonDecoder())
-				.target(Test.class, "http://121.11.161.36:8082");
-		return service.getLoginPage("张三");
-	}
+//	@RequestMapping("/login")
+//	@ResponseBody
+//	public String loginPage() {
+//		Test service = Feign.builder()
+//				// .encoder(new JacksonEncoder())
+//				// .decoder(new JacksonDecoder())
+//				.target(Test.class, "http://121.11.161.36:8082");
+//		return service.getLoginPage("张三");
+//	}
 }
